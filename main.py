7bafe076f6fd4b14ce3ae12c465e4a5963a591ce
Bùi Anh Dũng <bed209fit@gmail.com>
2025-04-21 20:50:28 +0700

    fix: thêm file index.html để render

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.secret_key = os.getenv("SECRET_KEY")
 # Route chính: render template index.html
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("blog.html")
 
 
 @app.route("/blog")
